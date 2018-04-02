403c0c2a774fc88d343e80d5682cd2b690e2d1f1
RodrigoEstay <restay2016@udec.cl>
2018-04-02 13:29:28 -0300

    avance 2 de abril

--- 2018-1/enProceso/136.c
+++ 2018-1/enProceso/136.c
@@ -1,35 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int isUgly (long long int n);
+
 int main (){
-	long long int n=2, div1, div2;
-	int pos=0, esFactorPrimo, k;
-	printf("1 ");
-	while(pos<20){
-		k=1;
-		printf("%lld ", n);
-		for(div1=2;div1<=n;div1++){
-			if(n%div1==0){
-				esFactorPrimo=1;
-				for(div2=2;div2<=n/2+1;div2++){
-					if(div1%div2==0){
-						esFactorPrimo=0;
-					}
-				}
-				if(esFactorPrimo==1){
-					printf("%lld ", div1);
-					if(div1!=2 && div1!=3 && div1!=5)
-						k=0;
-					}
-			}
-		}
-		if(k){
+	long long int n=2;
+	int pos=0;
+	printf("1\t%d\n", pos+1);
+	pos++;
+	while(pos<1500){
+		if(isUgly(n)){
+			printf("%lld\t%d\n", n, pos+1);
 			pos++;
-			printf("SE\n");
 		}
-		else
-			printf("NO\n");
 		n++;
 	}
 	return 0;
+}
+
+int isUgly (long long int n){
+	while(n%2==0)
+		n/=2;
+	while(n%3==0)
+		n/=3;
+	while(n%5==0)
+		n/=5;
+	if(n==1)
+		return 1;
+	else
+		return 0;	
 }
