c53190d7bd9ecf6352696495230b01eef707483d
RodrigoEstay <restay2016@udec.cl>
2018-04-18 13:18:23 -0300

    10363 hecho

--- 2018-1/aceptados/10363.c
+++ 2018-1/aceptados/10363.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Codigo hecho por mi cuenta. */
+
 int main (){
 	int N, cases, i, j, contX, contO, winX, winO;
+
+	/* Ocupamos una matriz de 3x4 ya que almacenaremos las lineas escaneadas como string
+	por lo tanto debe tener espacio para '\0'. */
+
 	char tictac[3][4];
 	scanf("%d", &N);
 	for(cases=0;cases<N;++cases, winO=0, winX=0, contX=0, contO=0){
 		scanf("%s%s%s", tictac[0], tictac[1], tictac[2]);
+
+		/* Contamos el numero de X y de O. */
+
 		for(i=0;i<3;++i){
 			for(j=0;j<3;++j){
 				if(tictac[i][j]=='X')
@@ -15,10 +24,18 @@ int main (){
 					++contO;
 			}
 		}
+
+		/* Si el numero de O es mayor que X, entonces el juego de gato no es posible,
+		de la misma manera si el numero de X es mayor por dos unidades al numero de 
+		O, tampoco es posible. */
+
 		if(contX-contO<0 || contX-contO>=2){
 			printf("no\n");
 			continue;
 		}
+
+		/* Observamos si X o O ganan en alguna fila o columna, */
+
 		for(i=0;i<3;i++){
 			if(tictac[i][0]==tictac[i][1] && tictac[i][0]==tictac[i][2]){
 				if(tictac[i][0]=='X')
@@ -33,16 +50,20 @@ int main (){
 					++winO;
 			}
 		}
+
+		/* Observamos si X o O ganan en diagonal. */
+
 		if(tictac[0][0]==tictac[1][1] && tictac[0][0]==tictac[2][2] || tictac[0][2]==tictac[1][1] && tictac[0][2]==tictac[2][0]){
 			if(tictac[1][1]=='X')
 				++winX;
 			else if(tictac[1][1]=='O')
 				++winO;
 		}
-		if(winX+winO>1){
-			printf("no\n");
-			continue;
-		}
+
+		/* Si O gano, se debe cumplir que el numero de X y O deben ser iguales, Si X gana
+		el numero de X debe ser 1 mayor al numero de O. En casos contrarios el juego de gato
+		no es posible. */ 
+
 		if(winO==1 && contX!=contO){
 			printf("no\n");
 			continue;
@@ -51,6 +72,9 @@ int main (){
 			printf("no\n");
 			continue;
 		}
+
+		/* Si no se cumplieron ninguna condicion anterior, entonces el juego de gato si es posible. */
+
 		printf("yes\n");
 	}
 	return 0;
