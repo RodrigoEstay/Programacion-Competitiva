229289c53d1cd90ab82a68372a2b933e853b103c
RodrigoEstay <restay2016@udec.cl>
2018-04-18 15:14:50 -0300

    avance de 11223

---
+++ 2018-1/enProceso/11223.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char tra[100]={'A', 'B', 'C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '?', 39, '!', '/', '(', ')', '&', ':', ';', '=', '+', '-', '_', '"', '@'};
+
+void ye(void);
+
+int main (){
+	printf("%c\n", a);
+	ye();
+	return 0;
+}
+
+void ye(void){
+	printf("%c\n", a);
+}
