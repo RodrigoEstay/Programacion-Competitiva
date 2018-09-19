a15b3d3f227e64f45728e34cb6619cad8b7bb4ef
RodrigoEstay <restay2016@udec.cl>
2018-09-19 01:55:22 -0300

    sep

--- 2018-2/enProceso/UVA11286(sinComentar).cpp
+++ 2018-2/enProceso/UVA11286(sinComentar).cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
+#include <sstream> // estudiar esto (pasar de int a string).
 
 using namespace std;
 
 int main(){
-	
+	map<int[5],int> courses;
+	int n, tempC[5], i; 
+	while(1){
+		cin >> n;
+		if(n == 0) break;
+		for(i=0 ; i<n ; ++i){
+			cin >> tempC[0] >> tempC[1] >> tempC[2] >> tempC[3] >> tempC[4];
+			sort(tempC, tempC+5);
+			courses[tempC]++;
+		}
+
+	}
 	return 0;
 }
