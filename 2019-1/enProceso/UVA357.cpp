1f7aa8aa383ae34af799dcd31043de0d0d127483
Rodrigo Estay <RodrigoEstay>
2019-06-18 00:54:40 -0400

    rip

---
+++ 2019-1/enProceso/UVA357.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n, m, sum;
+	int coins[5] = {1, 5, 10, 25, 50};
+	int comb = new int[30001]();
+	comb[0] = 1;
+		for(int i=1 ; i<30001 ; ++i){
+			sum = 0;
+			
+		}
+	while(1){
+		cin << n;
+		if(getc(stdin) == EOF) break;
+
+	}
+	return 0;
+}
